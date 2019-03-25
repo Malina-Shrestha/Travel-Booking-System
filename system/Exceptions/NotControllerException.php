4076d1c0e816858a2a1c2fb2cf5a7731c1d63d12
malina <malinasht123@gmail.com>
2019-03-25 11:54:47 +0545

    MVC completed

---
+++ system/Exceptions/NotControllerException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: malina
+ * Date: 3/24/19
+ * Time: 10:23 PM
+ */
+
+namespace System\Exceptions;
+
+
+class NotControllerException extends \Exception
+{
+
+}
